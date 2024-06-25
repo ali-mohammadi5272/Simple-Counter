512625a89c751fea219852477fb198bcd7197304
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-06-25 21:57:51 +0330

    feat: selecting DOM Elemens

---
+++ app.js
@@ -0,0 +1,4 @@
+const countElem = document.getElementsByClassName("counter__count")[0];
+const resetBtn = document.getElementsByClassName("counter__reset")[0];
+const incBtn = document.getElementsByClassName("counter__increaseBtn")[0];
+const decBtn = document.getElementsByClassName("counter__decreaseBtn")[0];
