68cef80f4286b6a58354cf169e8024fa327eba06
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-06-25 21:59:13 +0330

    feat: add Redux Actions

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const countElem = document.getElementsByClassName("counter__count")[0];
 const resetBtn = document.getElementsByClassName("counter__reset")[0];
 const incBtn = document.getElementsByClassName("counter__increaseBtn")[0];
 const decBtn = document.getElementsByClassName("counter__decreaseBtn")[0];
+//
+//
+//
+//
+// Actions
+const incrementAction = { type: "INCREMENT" };
+const decrementAction = { type: "DECREMENT" };
+const resetAction = { type: "RESET" };
